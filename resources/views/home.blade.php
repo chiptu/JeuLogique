f8237eb0f55b3aa150a4da82981a0e1084fe8120
alex <alexle-guillou@outlook.fr>
2021-03-25 10:19:50 +0100

    timer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,14 +23,126 @@
     </div>
 
     
-    <form action="/rocket/1" class="flex  justify-center">
-        <input type="image"  src = "{{ url('/') }}/images/42.png" class ="relative border rounded p-0 flex flex-col justify-between items-center w-1/4 h-1/4 bg-white 
+    <form action="/rocket/1" class="flex  justify-center fade-in">
+        <input type="image"  id="c4fa" src = "{{ url('/') }}/images/tesla.jpg" class ="relative border rounded p-0 flex flex-col justify-between items-center w-1/2 h-1/2 bg-white 
         transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110  "/>
     </form>
 
     
 
+    <footer class="bg-gray-500 absolute bottom-0 w-full">
+    
+
+        <div class ="flex h-1/2 w-full items-center justify-center ">
+
+
+        <div class="text-3xl"id="countdown" style="font-family:OCR A Std, monospace;">
+            
+            <span id="days"></span> jours</li>
+            <span id="hours"></span>h</li>
+            
+            
+        </div>
+
+
+        <i class="ml-5 fab fa-steam fa-3x"></i>
+
+            
+
+        </div>
+    
+    </footer>
+
+
+
     </body>
 
     
 </html>
+
+
+<style>
+.fade-in {
+animation: fadeIn ease 5s;
+-webkit-animation: fadeIn ease 5s;
+-moz-animation: fadeIn ease 5s;
+-o-animation: fadeIn ease 5s;
+-ms-animation: fadeIn ease 5s;
+}
+@keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+
+@-moz-keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+
+@-webkit-keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+
+@-o-keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+
+@-ms-keyframes fadeIn {
+0% {opacity:0;}
+100% {opacity:1;}
+}
+</style>
+
+
+<script>
+
+///TIMER
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  let birthday = "May 5, 2021 00:00:00",
+      countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour));
+          //document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          //document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        
+        //seconds
+      }, 0)
+  }());
+
+/// FADE IMAGE
+var i=0;
+var tabImage =["https://jeu.test/images/tesla.jpg","https://jeu.test/images/vaisseau.jpg"];
+var intervalId = window.setInterval(function(){
+    i++;
+    console.log({i,tabImage});
+    $("#c4fa").fadeTo("slow",1, function() {
+      $("#c4fa").attr("src",tabImage[i%2]);
+      
+     
+
+      /*$("#c4fa").removeClass("fade-in");
+      
+      void $("#c4fa").offsetWidth;
+  
+      $("#c4fa").addClass("fade-in");*/
+
+      //$("#c4fa").removeClass("fade-in").addClass("fade-in");
+  })
+
+}, 5000);
+  
+
+</script>
